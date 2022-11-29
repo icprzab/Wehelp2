84a2c6184eb7cf3d01d7a206e820b10ce9ff3bf6
icprzab <icprzab@gmail.com>
2022-11-29 20:27:18 +0800

    test

--- taipei-day-trip/static/attraction.js
+++ taipei-day-trip/static/attraction.js
@@ -1,29 +1,41 @@
 var page = 0;
-var pageCAT = 0;
+var inputAttraction = document.getElementById("inputAttraction").value;
 
+function getCategories() {
+    fetch("/api/categories")
+        .then(function (response) {
+            response.json()
+                .then(function (data) {
+                    let dataLength3 = data.data.length;
+                    for (let i = 0; i < dataLength3; i++) {
+                        let categories = document.createElement("button");
+                        categories.textContent = data.data[i];
+                        categories.setAttribute("onclick", `inputCategory("${data.data[i]}")`);
+                        document.getElementById("dialog").appendChild(categories).setAttribute("class", "flexbox");
+                    }
+                })
+        })
+}
+getCategories();
 
-// function getCategories() {
-//     fetch("/api/categories")
-//         .then(function (response) {
-//             response.json()
-//                 .then(function (data) {
-//                     let dataLength3 = data.data.length;
-//                     for (let i = 0; i < dataLength; i++) {
-//                         let categories = document.createElement("div");
-//                         document.getElementById("flex-container2").appendChild(categories).setAttribute("class", "flexbox2");
+function inputCategory(inputData) {
+    var input = document.getElementById("inputAttraction");
+    input.value = inputData;
+};
 
-//                         let img = document.createElement("div");
-//                         attractions.appendChild(img).setAttribute("class", "flexbox-image");
-//                     }
-//                 })
-//         })
-// }
+var dialog = document.getElementById("dialog");
+function showDialog() {
+    dialogOutside.style.display = "flex";
+    dialog.style.display = "flex";
+}
 
-// getCategories();
-
+function closeDialog() {
+    dialogOutside.style.display = "none";
+    dialog.style.display = "none";
+}
 
 function getData() {
-    fetch("/api/attractions?page=" + page)
+    fetch("/api/attractions?keyword=" + inputAttraction + "&page=" + page)
         .then(function (response) {
             response.json()
                 .then(function (data) {
@@ -33,7 +45,7 @@ function getData() {
                         let dataLength = data.data.length;
                         for (let i = 0; i < dataLength; i++) {
                             let attractions = document.createElement("div");
-                            document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
+                            document.getElementById("flex-container2").appendChild(attractions).setAttribute("class", "flexbox2");
 
                             let img = document.createElement("img");
                             img.src = data.data[i].images[0];
@@ -64,7 +76,7 @@ function getData() {
                         for (let i = 0; i < dataLength; i++) {
 
                             let attractions = document.createElement("div");
-                            document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
+                            document.getElementById("flex-container2").appendChild(attractions).setAttribute("class", "flexbox2");
 
                             let img = document.createElement("img");
                             img.src = data.data[i].images[0];
@@ -93,229 +105,27 @@ function getData() {
                     }
                 })
         })
-
 }
 
 var options = {
     rootMargin: "0px 0px 0px 0px",
     threshold: 0.5
 };
-let observer = new IntersectionObserver(handleIntersect, options);
+
+var observer = new IntersectionObserver(handleIntersect, options);
 observer.observe(document.getElementsByClassName("footer")[0]);
+
 function handleIntersect(entries) {
     if (entries[0].isIntersecting) {
         getData();
     }
 };
 
-var dialog = document.getElementById("dialog");
-function showDialog() {
-    dialog.style.display = "block";
-}
-
-function closeDialog() {
-    dialog.style.display = "none";
-}
-
 document.getElementById("taipei101Button").addEventListener("click", function () {
-    observer.disconnect();
-    pageCAT = 0;
-
-    if (typeof entries !== "undefined") {
-        observer2.disconnect();
-        document.getElementById("flex-container").innerHTML = "";
-
-        let observer2 = new IntersectionObserver(handleIntersect, options);
-        observer2.observe(document.getElementsByClassName("footer")[0]);
-        function handleIntersect(entries) {
-            if (entries[0].isIntersecting) {
-                getData2();
-                console.log("一")
-            }
-        };
-
-        function getData2() {
-            console.log("二")
-            let inputAttraction = document.getElementById("inputAttraction").value;
-            console.log(inputAttraction)
-
-            fetch("/api/attractions?keyword=" + inputAttraction + "&page=" + pageCAT)
-                .then(function (response) {
-                    response.json()
-                        .then(function (data) {
-                            console.log("三")
-                            if (data.nextPage === null) {
-                                console.log("四")
-                                observer2.unobserve(document.getElementsByClassName("footer")[0]);
-                                pageCAT = 0;
-                                let dataLength2 = data.data.length;
-                                for (let i = 0; i < dataLength2; i++) {
-                                    let attractions = document.createElement("div");
-                                    attractions.setAttribute("id", "flexbox");
-                                    document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
-
-                                    let img = document.createElement("img");
-                                    img.src = data.data[i].images[0];
-                                    attractions.appendChild(img).setAttribute("class", "flexbox-image");
-
-                                    let attractionsData = document.createElement("div");
-                                    attractions.appendChild(attractionsData).setAttribute("class", "attractionsBottom");
-
-                                    let attractionsBottomMRT = document.createElement("div");
-                                    attractionsBottomMRT.textContent = data.data[i].mrt;
-                                    attractionsData.appendChild(attractionsBottomMRT).setAttribute("class", "attractionsBottomMRT");
-
-                                    let attractionsBottomCAT = document.createElement("div");
-                                    attractionsBottomCAT.textContent = data.data[i].category;
-                                    attractionsData.appendChild(attractionsBottomCAT).setAttribute("class", "attractionsBottomCAT");
-
-                                    let attractionsMiddle = document.createElement("div");
-                                    attractions.appendChild(attractionsMiddle).setAttribute("class", "attractionsMiddle");
-
-                                    let attractionsMiddleText = document.createElement("div");
-                                    attractionsMiddleText.textContent = data.data[i].name;
-                                    attractionsMiddle.appendChild(attractionsMiddleText).setAttribute("class", "attractionsMiddleText");
-                                }
-                                console.log("五")
-                            }
-
-                            else if (data.nextPage !== null) {
-                                let dataLength2 = data.data.length;
-                                console.log("六")
-                                for (let i = 0; i < dataLength2; i++) {
-                                    let attractions = document.createElement("div");
-                                    attractions.setAttribute("id", "flexbox");
-                                    document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
-
-                                    let img = document.createElement("img");
-                                    img.src = data.data[i].images[0];
-                                    attractions.appendChild(img).setAttribute("class", "flexbox-image");
-
-                                    let attractionsData = document.createElement("div");
-                                    attractions.appendChild(attractionsData).setAttribute("class", "attractionsBottom");
-
-                                    let attractionsBottomMRT = document.createElement("div");
-                                    attractionsBottomMRT.textContent = data.data[i].mrt;
-                                    attractionsData.appendChild(attractionsBottomMRT).setAttribute("class", "attractionsBottomMRT");
-
-                                    let attractionsBottomCAT = document.createElement("div");
-                                    attractionsBottomCAT.textContent = data.data[i].category;
-                                    attractionsData.appendChild(attractionsBottomCAT).setAttribute("class", "attractionsBottomCAT");
-
-                                    let attractionsMiddle = document.createElement("div");
-                                    attractions.appendChild(attractionsMiddle).setAttribute("class", "attractionsMiddle");
-
-                                    let attractionsMiddleText = document.createElement("div");
-                                    attractionsMiddleText.textContent = data.data[i].name;
-                                    attractionsMiddle.appendChild(attractionsMiddleText).setAttribute("class", "attractionsMiddleText");
-                                }
-                                pageCAT = data.nextPage;
-                                console.log("七")
-                            }
-
-                        })
-                })
-
-        }
-    }
-
-
-    else if (typeof entries === "undefined") {
-        document.getElementById("flex-container").innerHTML = "";
-
-        let observer2 = new IntersectionObserver(handleIntersect, options);
-        observer2.observe(document.getElementsByClassName("footer")[0]);
-        function handleIntersect(entries) {
-            if (entries[0].isIntersecting) {
-                getData2();
-                console.log("1")
-            }
-        };
-
-        function getData2() {
-            console.log("2")
-            let inputAttraction = document.getElementById("inputAttraction").value;
-
-            fetch("/api/attractions?keyword=" + inputAttraction + "&page=" + pageCAT)
-                .then(function (response) {
-                    response.json()
-                        .then(function (data) {
-                            console.log("3")
-                            if (data.nextPage === null) {
-                                console.log("4")
-                                observer2.unobserve(document.getElementsByClassName("footer")[0]);
-                                pageCAT = 0;
-                                let dataLength2 = data.data.length;
-                                for (let i = 0; i < dataLength2; i++) {
-                                    let attractions = document.createElement("div");
-                                    attractions.setAttribute("id", "flexbox");
-                                    document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
-
-                                    let img = document.createElement("img");
-                                    img.src = data.data[i].images[0];
-                                    attractions.appendChild(img).setAttribute("class", "flexbox-image");
-
-                                    let attractionsData = document.createElement("div");
-                                    attractions.appendChild(attractionsData).setAttribute("class", "attractionsBottom");
-
-                                    let attractionsBottomMRT = document.createElement("div");
-                                    attractionsBottomMRT.textContent = data.data[i].mrt;
-                                    attractionsData.appendChild(attractionsBottomMRT).setAttribute("class", "attractionsBottomMRT");
-
-                                    let attractionsBottomCAT = document.createElement("div");
-                                    attractionsBottomCAT.textContent = data.data[i].category;
-                                    attractionsData.appendChild(attractionsBottomCAT).setAttribute("class", "attractionsBottomCAT");
-
-                                    let attractionsMiddle = document.createElement("div");
-                                    attractions.appendChild(attractionsMiddle).setAttribute("class", "attractionsMiddle");
-
-                                    let attractionsMiddleText = document.createElement("div");
-                                    attractionsMiddleText.textContent = data.data[i].name;
-                                    attractionsMiddle.appendChild(attractionsMiddleText).setAttribute("class", "attractionsMiddleText");
-                                }
-                                console.log("5")
-                            }
-
-                            else if (data.nextPage !== null) {
-                                let dataLength2 = data.data.length;
-                                console.log("6")
-                                for (let i = 0; i < dataLength2; i++) {
-                                    let attractions = document.createElement("div");
-                                    attractions.setAttribute("id", "flexbox");
-                                    document.getElementById("flex-container").appendChild(attractions).setAttribute("class", "flexbox");
-
-                                    let img = document.createElement("img");
-                                    img.src = data.data[i].images[0];
-                                    attractions.appendChild(img).setAttribute("class", "flexbox-image");
-
-                                    let attractionsData = document.createElement("div");
-                                    attractions.appendChild(attractionsData).setAttribute("class", "attractionsBottom");
-
-                                    let attractionsBottomMRT = document.createElement("div");
-                                    attractionsBottomMRT.textContent = data.data[i].mrt;
-                                    attractionsData.appendChild(attractionsBottomMRT).setAttribute("class", "attractionsBottomMRT");
-
-                                    let attractionsBottomCAT = document.createElement("div");
-                                    attractionsBottomCAT.textContent = data.data[i].category;
-                                    attractionsData.appendChild(attractionsBottomCAT).setAttribute("class", "attractionsBottomCAT");
-
-                                    let attractionsMiddle = document.createElement("div");
-                                    attractions.appendChild(attractionsMiddle).setAttribute("class", "attractionsMiddle");
-
-                                    let attractionsMiddleText = document.createElement("div");
-                                    attractionsMiddleText.textContent = data.data[i].name;
-                                    attractionsMiddle.appendChild(attractionsMiddleText).setAttribute("class", "attractionsMiddleText");
-                                }
-                                pageCAT = data.nextPage;
-                                console.log("7")
-                            }
-
-                        })
-                })
-
-        }
-    }
-
+    observer.unobserve(document.getElementsByClassName("footer")[0]);
+    page = 0;
+    document.getElementById("flex-container2").innerHTML = "";
+    observer.observe(document.getElementsByClassName("footer")[0]);
 })
 
 
